ac52f21c6ab74bf952e8610e3d8bec372d7a38a5
Ankush Hegde <59443849+ankush07261@users.noreply.github.com>
2024-04-06 09:50:50 +0530

    Update Experience.jsx

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,8 +1,15 @@
-import React, { useState } from "react";
 import "../css/experience.css";
 import "../css/mobileBtns.css";
 import { Link } from "react-router-dom";
 import { demoprojects, projects } from "../constants/Experience";
+import * as React from "react";
+import { useState } from "react";
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineDot from "@mui/lab/TimelineDot";
 
 function MyWork() {
   const [demo, setDemo] = useState(demoprojects);
@@ -18,22 +25,38 @@ function MyWork() {
 
         <h2>Professional Experience: </h2>
         <br />
-        <div className="projects prof-projects">
-          {project.map((project, index) => (
-            <div className="card" key={index}>
-              <img src={project.image} alt="project pic" />
-              <p>
-                <h3>{project.title}</h3>
-                <h4>Role: {project.role}</h4>
-                <h4>Description:</h4>
-                <p>{project.description}</p>
-                <b>Duration:</b>
-                <p>{project.duration}</p>
-              </p>
-            </div>
-          ))}
+        <div className="professional-experience">
+          <Timeline>
+            {project.map((project, index) => (
+              <TimelineItem key={index}>
+                <TimelineSeparator>
+                  <TimelineDot sx={{ backgroundColor: "yellow" }} />
+                  <TimelineConnector sx={{ backgroundColor: "yellow" }} />
+                </TimelineSeparator>
+                <TimelineContent>
+                  <div className="experience-card">
+                    <div className="card-header">
+                      <img
+                        className="exp-img"
+                        src={project.image}
+                        alt="project pic"
+                      />
+                      <h3>{project.title}</h3>
+                    </div>
+                    <p>
+                      <h4>Role: {project.role}</h4>
+                      <p>{project.duration}</p>
+                      <h4>Description:</h4>
+                      <p>{project.description}</p>
+                    </p>
+                  </div>
+                </TimelineContent>
+              </TimelineItem>
+            ))}
+          </Timeline>
         </div>
 
+        <br />
         <h2>Other / Demo Projects ...</h2>
         <br />
         <div className="projects">
@@ -44,7 +67,7 @@ function MyWork() {
                 <h3>{demo.title}</h3>
                 <br />
                 <p>{demo.description}</p>
-                <br/>
+                <br />
                 <a
                   href={demo.liveURL}
                   className="try-it"
